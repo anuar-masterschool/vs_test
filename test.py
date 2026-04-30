042889a86fb126f1babb01e3c43fdae6fd96da63
Anuar Santoyo <anuar.santoyoalum@campus.tu-berlin.de>
2026-04-30 19:36:02 +0200

    added pandas stuff

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+import pandas as pd
+
+# fetch a dataframe from an url
+url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+df = pd.read_csv(url)
+
+asdfdsafsa
+
+asdfsa
